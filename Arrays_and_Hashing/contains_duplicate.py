6d6c71d6d7015a070dfca74691041d2bd7e42c3e
Venchislaw <venchislavcodes@gmail.com>
2024-07-08 17:46:52 +0300

    Algorithmic solution

--- Arrays_and_Hashing/contains_duplicate.py
+++ Arrays_and_Hashing/contains_duplicate.py
@@ -1,3 +1,15 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return len(nums) != len(set(nums))
+
+# or 
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for value in nums:
+            if value in seen:
+                return True
+            seen.add(value)
+        
+        return False
