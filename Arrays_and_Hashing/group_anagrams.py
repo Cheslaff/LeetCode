3c239d7c8934bb29c51eccc35af8668a6373191a
Venchislaw <venchislavcodes@gmail.com>
2024-07-10 11:03:57 +0300

    add weird solution

--- Arrays_and_Hashing/group_anagrams.py
+++ Arrays_and_Hashing/group_anagrams.py
@@ -9,3 +9,26 @@ class Solution(object):
                 anagrams["".join(sorted(word))] = [word]
         
         return [anagrams[key] for key in anagrams.keys()]
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = []
+        checked = set()
+        
+        for l in range(len(strs)):
+            if strs[l] in checked:
+                continue
+
+            anagram = [strs[l]]
+            checked.add(strs[l])
+
+            for r in strs[l+1:]:
+                if len(r) != len(strs[l]): continue
+                if sorted(r) == sorted(strs[l]):
+                    anagram.append(r)
+                    checked.add(r)
+            
+            result.append(anagram)
+        
+        return result
