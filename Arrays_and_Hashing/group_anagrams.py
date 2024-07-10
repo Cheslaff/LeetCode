f9de35868643dc7af5bb68bcb5f5f7f829eb1831
Venchislaw <venchislavcodes@gmail.com>
2024-07-10 12:50:32 +0300

    removed weird solution
    It was too weird for this world... To weird and dumb...
    Utter trash...

--- Arrays_and_Hashing/group_anagrams.py
+++ Arrays_and_Hashing/group_anagrams.py
@@ -1,34 +1,15 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        anagrams = {}
-        
-        for word in strs:
-            if "".join(sorted(word)) in anagrams:
-                anagrams["".join(sorted(word))].append(word)
-            else:
-                anagrams["".join(sorted(word))] = [word]
-        
-        return [anagrams[key] for key in anagrams.keys()]
-
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result = []
-        checked = set()
-        
-        for l in range(len(strs)):
-            if strs[l] in checked:
-                continue
+        map_ = {}
 
-            anagram = [strs[l]]
-            checked.add(strs[l])
-
-            for r in strs[l+1:]:
-                if len(r) != len(strs[l]): continue
-                if sorted(r) == sorted(strs[l]):
-                    anagram.append(r)
-                    checked.add(r)
+        for str_ in strs:
+            sorted_str = "".join(sorted(str_))
+            if sorted_str not in map_:
+                map_[sorted_str] = [str_]
+            else:
+                map_[sorted_str].append(str_)
             
-            result.append(anagram)
+        result = [map_[key] for key in map_.keys()]
         
         return result
+            
