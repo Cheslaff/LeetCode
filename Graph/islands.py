6954f44bc65146c34b09e72875580a5350cc41a3
Venchislaw <venchislavcodes@gmail.com>
2024-07-13 10:11:44 +0300

    E

---
+++ Graph/islands.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == "1":
+                    self.dfs(grid, row, col)
+                    count += 1
+        return count
+
+    def dfs(self, grid, row, col):
+        if (min(row, col) < 0 or row >= len(grid)
+                or col >= len(grid[0]) or grid[row][col] != "1"):
+            return
+
+        grid[row][col] = "@"
+
+        self.dfs(grid, row + 1, col)
+        self.dfs(grid, row - 1, col)
+        self.dfs(grid, row, col + 1)
+        self.dfs(grid, row, col - 1)
