bb85d179faae54b2f59305c65836423f6257f3d5
Venchislaw <venchislavcodes@gmail.com>
2024-07-27 11:32:33 +0300

    Updated to O(1) space solution

--- Arrays_and_Hashing/find_disappeared.py
+++ Arrays_and_Hashing/find_disappeared.py
@@ -1,3 +1,4 @@
+# O(n) time |  O(n) space
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         bucket = [0] * (len(nums) + 1)
@@ -6,3 +7,17 @@ class Solution:
             bucket[num] = 1
         
         return [i for i in range(1, len(bucket)) if bucket[i] == 0]
+
+
+# O(n) time |  O(1) space
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        for num in nums:
+            i = abs(num) - 1
+            nums[i] = -1 * abs(nums[i])
+
+        res = []
+        for i, num in enumerate(nums):
+            if num > 0:
+                res.append(i + 1)
+        return res
