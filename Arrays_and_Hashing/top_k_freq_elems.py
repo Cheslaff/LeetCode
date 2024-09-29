4b3f206e71239e3f79ed4faf8452cc40c3cafb10
Venchislaw <venchislavcodes@gmail.com>
2024-09-29 12:16:04 +0300

    SussyBaka inc.

--- Arrays_and_Hashing/top_k_freq_elems.py
+++ Arrays_and_Hashing/top_k_freq_elems.py
@@ -1,3 +1,4 @@
+# using HashMap
 class Solution(object):
     def topKFrequent(self, nums, k):
         nums_dict = {}
@@ -11,3 +12,30 @@ class Solution(object):
 
         nums_dict = [key for key, _ in sorted(nums_dict.items(), key=lambda item: item[1], reverse=True)]
         return nums_dict[:k]
+
+
+# Using MinHeap
+
+import heapq
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # form pairs (num, count)
+        occurrences = {}
+        for num in nums:
+            occurrences[num] = occurrences.get(num, 0) + 1
+        
+        # build heap of pairs (-count, num) we need - as we turn max heap into min heap
+        heap = []
+
+        for key, val in occurrences.items():
+            heapq.heappush(heap, (-val, key))
+        
+        # pop most frequent pairs (we need only num)
+        result = []
+        while k > 0:
+            result.append(heapq.heappop(heap)[1])
+            k -= 1
+        
+        return result
