c4b1484db0e126d6ddfe45fb983987dcec1c2bd8
Venchislaw <venchislavcodes@gmail.com>
2024-10-14 22:40:01 +0300

    Was I f...kn drunk when wrote the previous version?
    Idk

--- NeetCourse/shuffle_the_arr.py
+++ NeetCourse/shuffle_the_arr.py
@@ -1,12 +1,12 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        result = []
+        res = []
+
         l = 0
         r = n
 
-        while len(result) < len(nums):
-            result.append(nums[l])
-            result.append(nums[r])
-            l += 1
-            r += 1
-        return result
+        for i in range(n):
+            res.append(nums[l + i])
+            res.append(nums[r + i])
+        
+        return res
