01a8d42e2ffa8bb7bf53fe0b3e01afdb421cfc8b
Cheslaff <venchislavcodes@gmail.com>
2024-12-29 17:35:14 +0600

    Gotta change it + I restart

--- NeetCourse/remove_dubs.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        unq = 1
-        for cur in range(1, len(nums)):
-            if nums[cur] != nums[cur-1]:
-                nums[unq] = nums[cur]
-                unq += 1
-        
-        return unq
